00bccf7619b35d99e7375678fe026f3462c0ed76
Manuel Wiltz <manuelwiltz@gmail.com>
2018-09-20 14:55:53 +0200

    Added Server Init Project

---
+++ backend/cookify/src/test/java/at/bhitm/cookify/CookifyApplicationTests.java
@@ -0,0 +1,16 @@
+package at.bhitm.cookify;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CookifyApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
