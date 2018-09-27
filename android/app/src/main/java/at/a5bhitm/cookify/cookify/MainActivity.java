1ec7adb81083397bd22c555ea0447c5a93c3bb56
Manuel Wiltz <manuelwiltz@gmail.com>
2018-09-27 21:09:10 +0200

    Added Splash Screen

--- android/app/src/main/java/at/a5bhitm/cookify/cookify/MainActivity.java
+++ android/app/src/main/java/at/a5bhitm/cookify/cookify/MainActivity.java
@@ -2,6 +2,7 @@ package at.a5bhitm.cookify.cookify;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.widget.CardView;
@@ -26,15 +27,6 @@ public class MainActivity extends AppCompatActivity
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
